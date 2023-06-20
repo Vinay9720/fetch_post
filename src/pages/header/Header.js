d33741923dc9e8edc2deaeb0560248a387ef6700
Vinay9720 <131252201+Vinay9720@users.noreply.github.com>
2023-06-20 15:24:48 +0530

    removed unnecessary code

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -27,9 +27,9 @@ function ResponsiveAppBar() {
   const dispatch = useDispatch();
   const homeClickHandler = (i) => {
     dispatch(Products_action());
-      i === 0 && navigate("/");
-      i === 1 && i !== 0 && i !== 2 && navigate("/products");
-      i === 2 && i !== 0 && i !== 1 && navigate("/cart");
+    i === 0 && navigate("/");
+    i === 1 && i !== 0 && i !== 2 && navigate("/products");
+    i === 2 && i !== 0 && i !== 1 && navigate("/cart");
   };
 
   const pages = ["Home", "Products", "Cart"];
@@ -76,49 +76,6 @@ function ResponsiveAppBar() {
                 navigate("/");
               }}
             />
-
-            {/* <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: "flex", md: "none" },
-              textAlign: "right",
-            }}
-          >
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
             <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
             <Typography
               variant="h5"
