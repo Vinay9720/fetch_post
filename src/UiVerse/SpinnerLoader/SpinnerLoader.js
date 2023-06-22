c2baffb4d5705994448f7f523daa9b5ad5716560
Vinay9720 <131252201+Vinay9720@users.noreply.github.com>
2023-06-22 08:12:47 +0530

    updated loader (spinner loader)

--- src/UiVerse/SpinnerLoader/SpinnerLoader.js
+++ src/UiVerse/SpinnerLoader/SpinnerLoader.js
@@ -1,12 +1,12 @@
 import React from "react";
 import classes from "./SpinnerLoader.module.css";
-import wineimg from "../../assests/Waiter serving 1.jpg";
+import wineGlass from "../../assests/wineGlass.png";
 
 function SpinnerLoader() {
   return (
     <div className={classes.loader}>
       <div>
-        <img src={wineimg} alt="wine" style={{ width: "100%" }} />
+        <img src={wineGlass} alt="wine" style={{ width: "100%" }} />
       </div>
     </div>
   );
